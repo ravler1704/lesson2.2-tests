d974226cd921f18a0009845ccc398ff228aa4dd2
Alexandr Raskatov <avr1704@yandex.ru>
2017-05-04 07:28:18 +0400

    Update list.php

--- list.php
+++ list.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 Error_reporting(E_ALL);
-if (isset($_FILES['filename']['tmp_name']) == true){
+if (isset($_FILES['filename']['tmp_name'])){
 	$file = $_FILES['filename']['tmp_name'];
 }
-if (isset($_FILES['filename']['name']) == true){
+if (isset($_FILES['filename']['name'])){
 	$filename = $_FILES['filename']['name'];
 }
 if (!empty($file)) {
@@ -21,7 +21,7 @@ if (!empty($file)) {
 	}
 	else {
 		if (copy($file, "uploads/".$filename)) {
-			echo "Файл " . $filename . " загружен!";
+			echo "Файл '" . $filename . "' загружен!";
 		}
 		else echo "Файл НЕ был загружен.";
 	}
@@ -31,19 +31,15 @@ echo "<h1>Загруженные тесты:</h1>";
 echo '<br />';
 echo '<br />';
 $dir    = 'uploads';
-$files = scandir($dir);
+$files = scandir($dir, 1);
 
 echo '<br />';
-foreach ($files as $key => $fileOne) {
-	if ($key > 1) {
-		echo $fileOne.'<br />';
+for ($i=0; $i<count($files)-2; $i++) {
+	echo $files[$i].' (Тест №  '.$i.')<a href="test.php?numTest=' . $i . '">Выбрать тест</a><br />';
 	}
-}
+
 echo '<br />';
 echo '<br />';
 echo '<br />';
 echo "<a href='admin.php'>Загрузить тест</a>";
-echo '<br />';
-echo "<a href='test.php?numTest=0'>Выбрать и пройти тест</a>";
-echo '<br />';
 ?>
